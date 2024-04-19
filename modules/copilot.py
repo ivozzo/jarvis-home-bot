49bd4d5eace3a3405197b0f2fceef047fd875902
Alessandro Antonello Ivaldi <aivaldi@imolainformatica.it>
2024-04-19 12:12:53 +0200

    Reporting after execution, better logging

--- modules/copilot.py
+++ modules/copilot.py
@@ -1,10 +1,13 @@
 import os
+
+from sydney.exceptions import NoResponseException, CreateConversationException
+
 import modules.utilities as utilities
 
 from sydney import SydneyClient
 
 
-async def get_alt_text_for_path(input_path, format, verbose=False, overwrite=False):
+async def get_alt_text_for_path(input_path, format, prompt, verbose=False, overwrite=False):
     output_path = "thumbnails/"
     if not os.path.exists(output_path):
         if verbose:
@@ -14,22 +17,51 @@ async def get_alt_text_for_path(input_path, format, verbose=False, overwrite=Fal
 
     print("Retrieving images")
     images = utilities.find_all_images(input_path, output_path, format, verbose, overwrite)
+    total_images = len(images)
+    processed_images = 0
+    error_images = 0
+
     print("-" * 40)
-    print("%s images retrieved and enqueued for elaboration" % len(images))
+    print("%s images retrieved and enqueued for elaboration" % total_images)
     print("-" * 40)
     for image in images:
-        response = await get_alt_text_for_image(image.get("thumbnail"), verbose)
-        utilities.generate_alt_text_file(input_path, image.get("filename"), format, response)
+        if verbose:
+            print("=" * 40)
+            print("Elaborating image: %s" % image.get("filename"))
+        try:
+            response = await get_alt_text_for_image(image.get("thumbnail"), prompt, verbose)
+            utilities.generate_alt_text_file(input_path, image.get("filename"), format, response)
+            processed_images = processed_images + 1
+        except NoResponseException as e:
+            print(e)
+            print("Please try again")
+            error_images = error_images + 1
+        except CreateConversationException as e:
+            print(e)
+            print("Please try again")
+            error_images = error_images + 1
+        if verbose:
+            print("=" * 40)
+            print()
+
+    print("""
+============ REPORT ============
+%s images found
+%s images processed
+%s images in error (please try again)
+================================
+""" % (total_images, processed_images, error_images))
 
 
-async def get_alt_text_for_image(path, verbose=False):
+async def get_alt_text_for_image(path, prompt, verbose=False):
     response = ""
     async with SydneyClient(style="precise") as sydney:
         if verbose:
-            print("Uploading image %s as attachment" % path)
-        response = await sydney.ask("Genera un alt text semplice per questa immagine", attachment=path)
+            print(" >>> Prompt asked: %s " % prompt)
+            print(" >>> Uploading image %s as attachment" % path)
+        response = await sydney.ask(prompt, attachment=path)
         if verbose:
-            print(" Response received: %s " % response)
+            print(" <<< Response received: %s " % response)
         await sydney.reset_conversation()
 
     return response
--- modules/utilities.py
+++ modules/utilities.py
@@ -15,20 +15,20 @@ def find_all_images(input_path, output_path, format, verbose=False, overwrite=Fa
                     # Check if thumbnail has already been created, if not or overwriting create it again
                     if check_if_thumbnail_already_exists(output_path, filename=f, verbose=verbose) and not overwrite:
                         if verbose:
-                            print(" Thumbnail already existing, skipping")
+                            print("     Thumbnail already existing, skipping")
                         thumb = output_path + f
                     else:
-                        print(" Generating thumbnail")
+                        print("     Generating thumbnail")
                         thumb = generate_thumbnails(input_path, output_path, f)
 
                     # Check if text has already been generated, if not or overwriting enqueue the item for generation
                     if not check_alt_file_existence(input_path, filename=f, format=format, verbose=verbose) or overwrite:
                         if verbose:
-                            print(" Enqueueing image for Alt Text generation")
+                            print("     Enqueueing image for Alt Text generation")
                         images.append(dict(thumbnail=thumb, filename=f))
                     else:
                         if verbose:
-                            print("Alt text has already been generated, skipping")
+                            print("     Alt text has already been generated, skipping")
     except FileNotFoundError:
         print("ERROR: you haven't specified a valid path!")
 
