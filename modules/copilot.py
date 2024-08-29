6533a498e1b05a2d1f063807b546aa09802f843d
Alessandro Antonello Ivaldi <aivaldi@imolainformatica.it>
2024-08-29 11:14:37 +0200

    Adding a results folder

--- modules/copilot.py
+++ modules/copilot.py
@@ -20,6 +20,18 @@ async def get_alt_text_for_path(input_path, format, prompt, verbose=False, overw
             print("thumbnail directory already existing, clearing: %s" % thumbnail_output_path)
         utilities.delete_all_files(thumbnail_output_path, verbose)
 
+    results_output_path = "results/"
+    if not os.path.exists(results_output_path):
+        if verbose:
+            print("-" * 40)
+            print("Creating results directory: %s" % results_output_path)
+        os.mkdir(results_output_path)
+    else:
+        if verbose:
+            print("-" * 40)
+            print("results directory already existing, clearing: %s" % results_output_path)
+        utilities.delete_all_files(results_output_path, verbose)
+
     print("Retrieving images")
     images = utilities.find_all_images(input_path, thumbnail_output_path, format, verbose, overwrite)
     total_images = len(images)
@@ -42,7 +54,7 @@ async def get_alt_text_for_path(input_path, format, prompt, verbose=False, overw
                 print(f"{tag:25}: {value}")
         try:
             response = await get_alt_text_for_image(image.get("thumbnail"), prompt, verbose)
-            utilities.generate_alt_text_file(input_path, image.get("filename"), format, response)
+            utilities.generate_alt_text_file(results_output_path, image.get("filename"), format, response)
             processed_images = processed_images + 1
         except NoResponseException as e:
             print(e)
